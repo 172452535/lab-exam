1070baadabdbfe062686a8936605158930fa9ae0
Nafizul Islam <65622090+172452535@users.noreply.github.com>
2020-08-08 11:06:10 +0600

    Create 1.c

---
+++ theory_exam1/1.c
@@ -0,0 +1,25 @@
+include<stdio.h>
+int CheckingOverflow(int tag);
+int main()
+{
+    int i;
+    int array[100];
+    int p = 0;
+    while(scanf("%d",&array[p]) != EOF)
+    {
+        p++;
+        if(CheckingOverflow(p)==0)
+        {
+            printf("Overflow\n");
+            break;
+        }
+    }
+    for(i = 0; i<p; i++)
+        printf("%d ", array[i]);
+    return 0;
+}
+int CheckingOverflow(int tag)
+{
+    if(tag+1>100)
+        return 0;
+}
